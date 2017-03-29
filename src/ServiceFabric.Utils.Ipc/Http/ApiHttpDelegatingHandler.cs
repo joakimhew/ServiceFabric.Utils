445a71c770d267eddb429c9455624b65ae39da5a
Joakim Hansson <joakim.hansson@tretton37.com>
2017-03-29 13:35:45 +0200

    Output httperrors to debug

--- src/ServiceFabric.Utils.Ipc/Http/ApiHttpDelegatingHandler.cs
+++ src/ServiceFabric.Utils.Ipc/Http/ApiHttpDelegatingHandler.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -38,6 +41,10 @@ namespace ServiceFabric.Utils.Ipc.Http
             if (!response.TryGetContentValue(out httpError))
                 return response;
 
+            DebugHttpErrors("HTTPERROR MAIN", httpError);
+            DebugHttpErrors("HTTPERROR INNER", httpError.InnerException);
+            DebugHttpErrors("HTTPERROR MODELSTATE", httpError.ModelState);
+
             Guid errorId;
 
             switch (response.StatusCode)
@@ -51,6 +58,37 @@ namespace ServiceFabric.Utils.Ipc.Http
             }
         }
 
+        private void DebugHttpErrors(string header, HttpError httpError)
+        {
+            Debug.WriteLine($"{Environment.NewLine}------ {header} start -------");
+
+            if (httpError == null)
+            {
+                Debug.WriteLine($"No HttpError for {header}");
+            }
+            else
+            {
+                WriteAllProperties();
+                WriteAllKeys();
+            }
+
+            Debug.WriteLine($"------ end {header} end -------{Environment.NewLine}");
+
+            void WriteAllProperties()
+            {
+                Debug.WriteLine($"Message: {httpError.Message}");
+                Debug.WriteLine($"Message details: {httpError.MessageDetail}");
+                Debug.WriteLine($"Exception message: {httpError.ExceptionMessage}");
+                Debug.WriteLine($"Exception type: {httpError.ExceptionType}");
+                Debug.WriteLine($"StackTrace: {httpError.StackTrace}");
+                Debug.WriteLine($"{Environment.NewLine}--- {header} KEYS ---");
+            }
+
+            void WriteAllKeys() => httpError.ToList().ForEach(x => { Debug.WriteLine($"KEY: {x.Key} VALUE: {x.Value}"); });
+        }
+
         public event Func<IOwinContext, HttpResponseMessage, HttpResponseMessage> ResponseMessageHandler;
     }
+    
+    
 }
