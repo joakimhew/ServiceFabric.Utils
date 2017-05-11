f93104484a3cab7e0bffae0fba88f163712ea318
Joakim Lindberg <joakim.lindberg@afconsult.com>
2017-05-11 11:30:36 +0200

    Update HttpContextExtensions with method ReadApiResponseMessageAs

--- src/ServiceFabric.Utils.Http/Extensions/HttpContentExtensions.cs
+++ src/ServiceFabric.Utils.Http/Extensions/HttpContentExtensions.cs
@@ -6,10 +6,34 @@ namespace ServiceFabric.Utils.Http.Extensions
 {
     public static class HttpContentExtensions
     {
+        /// <summary>
+        /// Read as <see cref="ApiResponseMessage{TExpectedMessageType}"/>.
+        /// </summary>
+        /// <typeparam name="TExpectedMessageType"></typeparam>
+        /// <param name="content">The content.</param>
+        /// <returns>
+        /// <see cref="ApiResponseMessage{TExpectedMessageType}"/>.
+        /// </returns>
         public static async Task<ApiResponseMessage<TExpectedMessageType>> ReadAsApiResponseMessageAsync<TExpectedMessageType>(this HttpContent content)
         {
             var json = await content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<ApiResponseMessage<TExpectedMessageType>>(json);
+        }
+
+        /// <summary>
+        /// Read as <see cref="ApiResponseMessage{TExpectedMessageType}"/> and returns only <typeparamref name="TExpectedMessageType"/>.
+        /// </summary>
+        /// <typeparam name="TExpectedMessageType"></typeparam>
+        /// <param name="content">The content.</param>
+        /// <returns>
+        /// <typeparamref name="TExpectedMessageType"/>.
+        /// </returns>
+        public static async Task<TExpectedMessageType> ReadApiResponseMessageAs<TExpectedMessageType>(this HttpContent content)
+        {
+            var json = await content.ReadAsStringAsync();
+            var apiResponseMessage = JsonConvert.DeserializeObject<ApiResponseMessage<TExpectedMessageType>>(json);
+
+            return apiResponseMessage.Message;
         }
     }
 }
