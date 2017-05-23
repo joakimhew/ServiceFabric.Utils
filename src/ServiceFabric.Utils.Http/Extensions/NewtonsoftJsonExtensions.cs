f358fe786c7ddf79738c05c125db6be83c683f6b
Joakim hansson <joakim.hansson@tretton37.com>
2017-05-23 10:26:26 +0200

    Set default contract resolver for HttpContentExtensions to CamelCase

--- src/ServiceFabric.Utils.Http/Extensions/NewtonsoftJsonExtensions.cs
+++ src/ServiceFabric.Utils.Http/Extensions/NewtonsoftJsonExtensions.cs
@@ -2,14 +2,24 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Schema;
 using Newtonsoft.Json.Schema.Generation;
+using Newtonsoft.Json.Serialization;
 
 namespace ServiceFabric.Utils.Http.Extensions
 {
     public static class NewtonsoftJsonExtensions
     {
-        public static T TryParse<T>(this string json) where T : new()
+        public static T TryParse<T>(this string json)
         {
-            var generator = new JSchemaGenerator();
+            return TryParse<T>(json, new CamelCasePropertyNamesContractResolver());
+        }
+
+        public static T TryParse<T>(this string json, IContractResolver contractResolver)
+        {
+            var generator = new JSchemaGenerator
+            {
+                ContractResolver = contractResolver
+            };
+
             var schema = generator.Generate(typeof(T));
 
             try
